9b91d82a0ce6d2c88bce53e49037b744f8a3d634
AmitaiB <blickstein@gmail.com>
2016-01-27 12:14:25 -0500

    Adds Stack.h

---
+++ DataStructuresWithTests/Stack.h
@@ -0,0 +1,15 @@
+//
+//  Stack.h
+//  DataStructuresWithTests
+//
+//  Created by Amitai Blickstein on 1/27/16.
+//  Copyright © 2016 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Stack : NSObject
+
+-(void)push
+
+@end
