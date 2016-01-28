3318bbe6731d4404367a567dc014d3f98e876f53
AmitaiB <blickstein@gmail.com>
2016-01-28 04:10:09 -0500

    Adds contexts to tests

--- DataStructuresWithTestsTests/StackLLSpec.m
+++ DataStructuresWithTestsTests/StackLLSpec.m
@@ -23,30 +23,42 @@ describe(@"StackLL", ^{
     beforeEach(^{
 
     });
-    
-    it(@"can be created", ^{
-        expect(stack).toNot.beNil;
+    context(@"initialization", ^{
+        
+        it(@"can be created", ^{
+            expect(stack).toNot.beNil;
+        });
+
     });
     
-    it(@"can push a value onto the stack", ^{
-        [stack push:3];
-        expect([stack containsValue:3]).to.beTruthy;
+    context(@"testing push/pop methods", ^{
+        
+        it(@"can push a value onto the stack", ^{
+            [stack push:3];
+            expect([stack containsValue:3]).to.beTruthy;
+        });
+        
+        it(@"can pop a value from the stack", ^{
+            [stack push:4];
+            expect([stack pop]).to.equal(4);
+        });
+        
+        it(@"can push multiple values onto the stack", ^{
+            [stack push:5];
+            [stack push:7];
+            [stack printStack];
+            expect([stack containsValue:5] && [stack containsValue:7]).to.beTruthy;
+        });
+        
+        it(@"pushes new values onto the top of the stack", ^{
+            [stack push:11];
+            [stack push:13];
+            [stack push:15];
+            expect([stack pop]).to.equal(15);
+        });
+        
     });
-    
-    it(@"can push multiple values onto the stack", ^{
-        [stack push:5];
-        [stack push:7];
-        [stack printStack];
-        expect([stack containsValue:5] && [stack containsValue:7]).to.beTruthy;
-    });
-    
-    it(@"pushes new values onto the top of the stack", ^{
-        [stack push:11];
-        [stack push:13];
-        [stack push:15];
-        expect([stack pop]).to.equal(15);
-    });
-    
+        
     
     
     afterEach(^{
