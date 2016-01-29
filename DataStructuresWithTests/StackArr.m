5a9d3bc4c83388094af6aa26a4c0b07f799c4e0d
AmitaiB <blickstein@gmail.com>
2016-01-29 12:31:38 -0500

    Impls push, sort of the hard way

--- DataStructuresWithTests/StackArr.m
+++ DataStructuresWithTests/StackArr.m
@@ -9,7 +9,8 @@
 #import "StackArr.h"
 
 @interface StackArr()
-@property (nonatomic, strong) NSArray *array;
+@property (nonatomic, strong) NSMutableArray <NSNumber*> *array;
+@property (nonatomic) NSInteger itemsNum;
 @end
 
 @implementation StackArr
@@ -17,11 +18,58 @@
 -(instancetype)init {
     if (!(self = [super init])) return nil;
     
-    _array = [NSArray new];
+        // The array of items contained in the stack. Init to 0, will
+        // expand as needed during Push.
+    _array = [NSMutableArray arrayWithCapacity:0];
+
+        // Not the size of the array, but the number of items in the stack.
+    _itemsNum = 0;
+    
     
     return self;
 }
 
+-(void)push:(NSInteger)value {
+        // itemsNum = 0 ... first push
+        // itemsNum = length ... growth boundary
+    if (self.itemsNum == self.array.count) {
+        NSInteger newLength = self.itemsNum == 0 ? 4 : self.itemsNum * 2;
+        __block NSMutableArray <NSNumber*> *longerArray = [NSMutableArray arrayWithCapacity:newLength];
+     
+        [self.array enumerateObjectsUsingBlock:^(NSNumber * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            longerArray[idx] = obj;
+        }];
+        self.array = longerArray;
+    }
+    
+    self.array[self.itemsNum] = @(value);
+    self.itemsNum++;
+
+    //  The easy way:
+    //    [self.array addObject:@(value)];
+}
+
+-(NSInteger)pop {
+    NSInteger valueToReturn = self.array.lastObject.integerValue;
+    [self.array removeLastObject];
+    return valueToReturn;
+}
+
+-(NSString *)printStack {
+    __block NSMutableString *description = [@"" mutableCopy];
+    [self.array enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(NSNumber * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        
+    }];
+}
+
+
+-(NSInteger)indexOfValue:(NSInteger)aValue {
+    
+}
+
+-(BOOL)containsValue:(NSInteger)aValue {
+    
+}
 
 
 @end
