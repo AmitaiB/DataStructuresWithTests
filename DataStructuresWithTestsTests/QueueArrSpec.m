7f1f403d4068834a9f9f3fb3d560d6ee8574d77c
AmitaiB <blickstein@gmail.com>
2016-01-31 13:18:59 -0500

    Adds and impls QueueArrSpec and passes tests

---
+++ DataStructuresWithTestsTests/QueueArrSpec.m
@@ -0,0 +1,83 @@
+//
+//  QueueArrSpec.m
+//  DataStructuresWithTests
+//
+//  Created by Amitai Blickstein on 1/31/16.
+//  Copyright 2016 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <Specta.h>
+#import <Expecta.h>
+#import "QueueArr.h"
+
+
+SpecBegin(QueueArr)
+
+describe(@"QueueArr", ^{
+    __block QueueArr *queue;
+    
+    beforeAll(^{
+        queue = [QueueArr new];
+    });
+    
+    beforeEach(^{
+        
+    });
+    context(@"initialization", ^{
+        
+        it(@"can be created", ^{
+            expect(queue).toNot.beNil;
+        });
+        
+    });
+    
+    context(@"testing en/dequeue methods", ^{
+        
+        it(@"can enqueue a value", ^{
+            [queue enqueue:3];
+            expect([queue containsValue:3]).to.beTruthy;
+        });
+        
+        it(@"can enqueue multiple values", ^{
+            [queue enqueue:5];
+            [queue enqueue:7];
+            [queue printQueue];
+            expect([queue containsValue:5] && [queue containsValue:7]).to.beTruthy;
+        });
+        
+        it(@"dequeue will follow FIFO order", ^{
+            [queue enqueue:4];
+            [queue enqueue:6];
+            expect([queue containsValue:6] && [queue peek] == 4).to.beTruthy;
+        });
+        
+    });
+    
+    context(@"testing MISC methods", ^{
+        
+        it(@"can remove all values with removeAll()", ^{
+            [queue enqueue:111];
+            [queue enqueue:222];
+            [queue enqueue:333];
+            [queue removeAll];
+            expect(queue.isEmpty).to.beTruthy;
+            
+        });
+        
+        it(@"can peek at a value without altering it", ^{
+            
+        });
+    });
+    
+    
+    
+    afterEach(^{
+        
+    });
+    
+    afterAll(^{
+        
+    });
+});
+
+SpecEnd
