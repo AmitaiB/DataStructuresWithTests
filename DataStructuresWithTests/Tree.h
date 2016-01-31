9c073e90cde4289cfefb4e6960d521e91ad657d0
AmitaiB <blickstein@gmail.com>
2016-01-31 17:01:36 -0500

    Adds TreeSpec

---
+++ DataStructuresWithTests/Tree.h
@@ -0,0 +1,14 @@
+//
+//  Tree.h
+//  DataStructuresWithTests
+//
+//  Created by Amitai Blickstein on 1/31/16.
+//  Copyright © 2016 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TreeNode.h"
+
+@interface Tree : NSObject
+
+@end
---
+++ DataStructuresWithTestsTests/TreeSpec.m
@@ -0,0 +1,39 @@
+//
+//  TreeSpec.m
+//  DataStructuresWithTests
+//
+//  Created by Amitai Blickstein on 1/31/16.
+//  Copyright 2016 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <Specta.h>
+#import <Expecta.h>
+#import "Tree.h"
+
+
+SpecBegin(Tree)
+
+describe(@"Tree", ^{
+    
+    beforeAll(^{
+
+    });
+    
+    beforeEach(^{
+
+    });
+    
+    it(@"", ^{
+
+    });  
+    
+    afterEach(^{
+
+    });
+    
+    afterAll(^{
+
+    });
+});
+
+SpecEnd
